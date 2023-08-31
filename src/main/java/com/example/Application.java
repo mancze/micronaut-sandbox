5e823b9184303f9562196444b286534152bc1c95
Michal Novák <1493286+mancze@users.noreply.github.com>
2023-08-31 18:21:30 +0200

    Configure ssl and use custom keystore configuration

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,61 @@
 package com.example;
 
+import io.micronaut.context.annotation.*;
+import io.micronaut.http.ssl.DefaultSslConfiguration;
+import io.micronaut.http.ssl.SslConfiguration;
 import io.micronaut.runtime.Micronaut;
+import jakarta.inject.Inject;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 public class Application {
+
+	private static final Function<String, String> DECRYPTOR = (encrypted) -> encrypted.equals("_encrypted_") ? "1234" : encrypted;
 
 	public static void main(String[] args) {
 		Micronaut.run(Application.class, args);
 	}
 
+	@Context
+	static class KeyStoreDumper {
+		KeyStoreDumper(ReplacedKeyStoreConfiguration storeConfiguration) {
+			System.out.printf("keystore bean: %s%n", storeConfiguration.getClass().getName());
+			System.out.printf("keystore path: %s%n", storeConfiguration.getPath().orElse(null));
+			System.out.printf("keystore password: %s%n", storeConfiguration.getPassword().orElse(null));
+		}
+	}
+
+	@Secondary
+	@BootstrapContextCompatible
+	@ConfigurationProperties("micronaut.ssl.key-store")
+	@Replaces(DefaultSslConfiguration.DefaultKeyStoreConfiguration.class)
+	static class ReplacedKeyStoreConfiguration extends DefaultSslConfiguration.DefaultKeyStoreConfiguration {
+
+		@Inject
+		ReplacedKeyStoreConfiguration() {
+		}
+
+		@Override
+		public Optional<String> getPassword() {
+			return super.getPassword()
+					.map(DECRYPTOR);
+		}
+	}
+
+	@Primary
+	@BootstrapContextCompatible
+	@ConfigurationProperties("micronaut.ssl.key-store")
+	@Replaces(DefaultSslConfiguration.DefaultKeyStoreConfiguration.class)
+	static class ExtendBaseClassKeyStoreConfiguration extends SslConfiguration.KeyStoreConfiguration {
+		@Inject
+		ExtendBaseClassKeyStoreConfiguration() {
+		}
+
+		@Override
+		public Optional<String> getPassword() {
+			return super.getPassword()
+					.map(DECRYPTOR);
+		}
+	}
 }
---
+++ src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import io.micronaut.http.ssl.DefaultSslConfiguration;
+import io.micronaut.http.ssl.SslConfiguration;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@MicronautTest(startApplication = false)
+class ApplicationTest {
+
+	@Inject
+	DefaultSslConfiguration defaultSslConfiguration;
+
+	@Inject
+	Application.ReplacedKeyStoreConfiguration keyStoreConfiguration;
+
+	@Inject
+	Application.ExtendBaseClassKeyStoreConfiguration extendBaseClassKeyStoreConfiguration;
+
+	@Test
+	void keyStoreConfiguration_shouldHaveExpectedValues() {
+		assertThat(keyStoreConfiguration.getClass().getName()).isEqualTo("com.example.Application$ReplacedKeyStoreConfiguration");
+		assertKeyStoreConfiguration(keyStoreConfiguration);
+	}
+
+	@Test
+	void noReplaceNoExtendsKeyStoreConfiguration_shouldHaveExpectedValues() {
+		assertThat(extendBaseClassKeyStoreConfiguration.getClass().getName()).isEqualTo("com.example.Application$ExtendBaseClassKeyStoreConfiguration");
+		assertKeyStoreConfiguration(extendBaseClassKeyStoreConfiguration);
+	}
+
+	@Test
+	void defaultSslConfiguration_shouldUseReplacedBean() {
+		assertThat(defaultSslConfiguration.getKeyStore().getClass().getName()).isEqualTo("com.example.Application$ReplacedKeyStoreConfiguration");
+	}
+
+	private void assertKeyStoreConfiguration(SslConfiguration.KeyStoreConfiguration keyStoreConfiguration1) {
+		assertThat(keyStoreConfiguration1.getPath()).hasValue("classpath:keystore.p12");
+		assertThat(keyStoreConfiguration1.getPassword()).hasValue("1234");
+	}
+}
